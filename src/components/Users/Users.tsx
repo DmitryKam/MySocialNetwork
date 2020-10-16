737bb093d3db9dad4bd19e274009ee8be1575b4f
Дмитрий <@DIMA>
2020-10-16 20:25:22 +0300

    51 end

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {UsersType} from '../../redux/users-reducer';
-import s from './Users.module.css'
+import s from './Users.module.css';
+import axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 type UsersPropsType = {
     users: UsersType[]
@@ -12,35 +14,13 @@ type UsersPropsType = {
 
 
 function Users(props: UsersPropsType) {
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            debugger;
+            props.setUsers(response.data.items);
+        });
 
-    if(props.users.length ===0){
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'https://microhealth.com/assets/images/illustrations/personal-user-illustration-@2x.png',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I Am a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl: 'https://microhealth.com/assets/images/illustrations/personal-user-illustration-@2x.png',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I Am a boss too',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://microhealth.com/assets/images/illustrations/personal-user-illustration-@2x.png',
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I Am a boss too',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
 
-        ]);
     }
 
 
@@ -50,7 +30,11 @@ function Users(props: UsersPropsType) {
                 props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={s.userPhoto}/>
+                    <img
+                        src={u.photos.small != null
+                            ? u.photos.small
+                            : userPhoto}
+                        className={s.userPhoto}/>
                 </div>
                 <div>
                     {u.followed
@@ -66,12 +50,12 @@ function Users(props: UsersPropsType) {
             </span>
                     <span>
                  <span>
-                     <div>{u.fullName}</div>
+                     <div>{u.name}</div>
                      <div>{u.status}</div>
                  </span>
                  <span>
-                     <div>{u.location.country}</div>
-                     <div>{u.location.city}</div>
+                     <div>{'u.location.country'}</div>
+                     <div>{'u.location.city'}</div>
                  </span>
                 </span>
                 </div>)
