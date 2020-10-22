64232b4f1455a29d6a43530d652e078042259fb3
Дмитрий <@DIMA>
2020-10-22 19:03:42 +0300

    53 end + photo

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -14,15 +14,13 @@ type UsersPropsType = {
 
 class Users extends React.Component<UsersPropsType> {
 
-    constructor(props:UsersPropsType) {    //????
-        super(props);
+    componentDidMount(): void {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
-            debugger;
-            this.props.setUsers(response.data.items);
-        });
+                debugger;
+                this.props.setUsers(response.data.items);
+    })
     }
-
     render() {
         return (
             <div>
